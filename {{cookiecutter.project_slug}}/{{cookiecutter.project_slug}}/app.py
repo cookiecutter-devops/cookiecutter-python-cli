06db9f57fdb8b187249667f38e2e019893c46d63
hujianli94 <1879324764@qq.com>
2026-01-09 17:23:36 +0800

    add code

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/app.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/app.py
@@ -9,15 +9,15 @@ from rich.traceback import install
 from {{cookiecutter.project_slug}}.commands import init, show
 from . import __version__, log
 
-@click.group()
-@click.command(context_settings={"auto_envvar_prefix": "{{cookiecutter.package_name}}"})
+@click.group(context_settings={"auto_envvar_prefix":  "{{cookiecutter.package_name}}"})
 @click.option("-l", "--log-level", type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], case_sensitive=False), default="INFO", help="Log level", show_default=True)
 @click.option("--log-file", type=click.Path(dir_okay=False, writable=True, path_type=Path), help="Log file")
 @click.version_option(__version__)
 def cli(log_level, log_file):
     if log_file:
         file_handler = logging.FileHandler(log_file, mode="w")
-        file_handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(message)s"))
+        formatter = logging.Formatter('%(asctime)s - %(name)s - "%(pathname)s:%(lineno)d" - %(levelname)s - %(message)s',"%Y-%m-%d %H:%M:%S")
+        file_handler.setFormatter(formatter)
         log.addHandler(file_handler)
 
     log.setLevel(log_level)
